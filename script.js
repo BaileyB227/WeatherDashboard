f8d725ce09d83737422f3d20f88fbaa681f76adf
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-15 16:58:24 -0500

    got search function working

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let apiKey = "3d47311119b7b5ec8772c781e9cdddce";
-let queryUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`
-fetch(queryUrl)
-.then(headers => headers.json())
-.then(weatherData => {
-    console.log(weatherData);
+let city = "";
+
+$("#search-btn").on("click", function(event){
+    event.preventDefault();
+    city = $("#search-city").val();
+    getWeather();
+    cityHistory();
+    storeLocal();
 })
+
+function getWeather() {
+    let latlonUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    
+    fetch(latlonUrl)
+    .then(headers => headers.json())
+    .then(cityData => {
+        let cityLat = cityData.city.coord.lat;
+        let cityLon = cityData.city.coord.lon;
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`
+        
+        fetch(weatherUrl)
+        .then(headers => headers.json())
+        .then(weatherData =>{
+            console.log(weatherData)
+        })
+    })
+}
