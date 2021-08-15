a53b4af2eeccf0d543e48e1cf4e7a4c91bc10dd4
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-15 18:27:19 -0500

    finished JS for text at top, started on bottom cards

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let city = "";
 
 $("#search-btn").on("click", function(event){
     event.preventDefault();
-    city = $("#search-city").val();
+    city = $("#search-city").val().trim();
     getWeather();
-    cityHistory();
-    storeLocal();
 })
 
 function getWeather() {
@@ -17,12 +15,39 @@ function getWeather() {
     .then(cityData => {
         let cityLat = cityData.city.coord.lat;
         let cityLon = cityData.city.coord.lon;
-        let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`
+        let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${apiKey}`
         
         fetch(weatherUrl)
         .then(headers => headers.json())
         .then(weatherData =>{
-            console.log(weatherData)
+
+            let currentIcon = weatherData.current.weather[0].icon
+            let currentUvi = weatherData.current.uvi
+
+            $("#currentCity").text(city)
+            $("#currentDate").text(moment().format("(M/D/YYYY)"));
+            $("#currentIcon").attr("src", "https://openweathermap.org/img/w/" + currentIcon + ".png");
+            $("#currentTemp").text(weatherData.current.temp);
+            $("#currentWind").text(weatherData.current.wind_speed);
+            $("#currentHumidity").text(weatherData.current.humidity);
+            $("#currentUV").text(currentUvi);
+
+            if (currentUvi>=0 && currentUvi<3) {
+                $("#currentUV").css('background-color', 'green')
+            } else if (currentUvi>=3 && currentUvi<6){
+                $("#currentUV").css('background-color', 'yellow')
+            } else {
+                $("currentUV").css('background-color', 'red')
+            };
+
+            $("#date1").text(moment(weatherData.daily.dt).format("M/D/YYYY"));
+            $("weather1")
+            $("temp1")
+            $("wind1")
+            $("humidity1")
+
+            console.log(moment(weatherData.daily[1].dt).format("M/D/YYYY"))
+            console.log(weatherData.daily[1].dt)
         })
     })
 }
