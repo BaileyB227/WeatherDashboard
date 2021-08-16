8cff2543c13aa8e478ce0b4d196f3b392fabb4fc
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-15 20:26:16 -0500

    got all cards filling and list for history added

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ $("#search-btn").on("click", function(event){
     event.preventDefault();
     city = $("#search-city").val().trim();
     getWeather();
+    cityList();
+    saveSearch();
 })
+
+$('#city-list').on("click", (event) => {
+    event.preventDefault();
+    $('#search-city').val(event.target.textContent);
+    city = $('#search-city').val();
+    getWeather();
+});
 
 function getWeather() {
     let latlonUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
@@ -16,12 +25,17 @@ function getWeather() {
         let cityLat = cityData.city.coord.lat;
         let cityLon = cityData.city.coord.lon;
         let weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${apiKey}`
-        
+
         fetch(weatherUrl)
         .then(headers => headers.json())
         .then(weatherData =>{
 
             let currentIcon = weatherData.current.weather[0].icon
+            let weatherIcon1 = weatherData.daily[1].weather[0].icon
+            let weatherIcon2 = weatherData.daily[2].weather[0].icon
+            let weatherIcon3 = weatherData.daily[3].weather[0].icon
+            let weatherIcon4 = weatherData.daily[4].weather[0].icon
+            let weatherIcon5 = weatherData.daily[5].weather[0].icon
             let currentUvi = weatherData.current.uvi
 
             $("#currentCity").text(city)
@@ -40,14 +54,47 @@ function getWeather() {
                 $("currentUV").css('background-color', 'red')
             };
 
-            $("#date1").text(moment(weatherData.daily.dt).format("M/D/YYYY"));
-            $("weather1")
-            $("temp1")
-            $("wind1")
-            $("humidity1")
+            $("#date1").text(moment.unix(weatherData.daily[1].dt).format("M/D/YYYY"));
+            $("#weather1").attr("src", "https://openweathermap.org/img/w/" + weatherIcon1 + ".png")
+            $("#temp1").text(weatherData.daily[1].temp.day)
+            $("#wind1").text(weatherData.daily[1].wind_speed)
+            $("#humidity1").text(weatherData.daily[1].humidity)
 
-            console.log(moment(weatherData.daily[1].dt).format("M/D/YYYY"))
-            console.log(weatherData.daily[1].dt)
+            $("#date2").text(moment.unix(weatherData.daily[2].dt).format("M/D/YYYY"));
+            $("#weather2").attr("src", "https://openweathermap.org/img/w/" + weatherIcon2 + ".png")
+            $("#temp2").text(weatherData.daily[2].temp.day)
+            $("#wind2").text(weatherData.daily[2].wind_speed)
+            $("#humidity2").text(weatherData.daily[2].humidity)
+
+            $("#date3").text(moment.unix(weatherData.daily[3].dt).format("M/D/YYYY"));
+            $("#weather3").attr("src", "https://openweathermap.org/img/w/" + weatherIcon3 + ".png")
+            $("#temp3").text(weatherData.daily[3].temp.day)
+            $("#wind3").text(weatherData.daily[3].wind_speed)
+            $("#humidity3").text(weatherData.daily[3].humidity)
+
+            $("#date4").text(moment.unix(weatherData.daily[4].dt).format("M/D/YYYY"));
+            $("#weather4").attr("src", "https://openweathermap.org/img/w/" + weatherIcon4 + ".png")
+            $("#temp4").text(weatherData.daily[4].temp.day)
+            $("#wind4").text(weatherData.daily[4].wind_speed)
+            $("#humidity4").text(weatherData.daily[4].humidity)
+
+            $("#date5").text(moment.unix(weatherData.daily[5].dt).format("M/D/YYYY"));
+            $("#weather5").attr("src", "https://openweathermap.org/img/w/" + weatherIcon5 + ".png")
+            $("#temp5").text(weatherData.daily[5].temp.day)
+            $("#wind5").text(weatherData.daily[5].wind_speed)
+            $("#humidity5").text(weatherData.daily[5].humidity)
         })
     })
+}
+
+function cityList() {
+    let cityHistory = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`
+    
+    if (city !== ""){
+        $("#city-list").append(cityHistory)
+    }
+}
+
+function saveSearch() {
+    city = $("#search-city").val().trim();
 }
